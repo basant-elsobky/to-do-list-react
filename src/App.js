08c33b3ea5b2b15d0d8d1e98bcc2fc04d5cc1259
basant-elsobky <124135940+basant-elsobky@users.noreply.github.com>
2023-12-05 18:08:25 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+import Todo from './component/Todo';
+
+function App() {
+  
+  return (
+    <>
+
+
+
+
+<Todo />
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Todo.js
@@ -0,0 +1,96 @@
+import React, { useRef, useState } from 'react'
+import './todo.css'
+
+import { faXmark, faCheck, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import '@fortawesome/fontawesome-free/css/all.min.css';
+function Todo() {
+    const input = useRef()
+    const parg = useRef()
+    const addbtn = useRef()
+    const [todos, settodos] = useState([]);
+
+
+
+
+    function add(event) {
+        event.preventDefault();
+        const text = input.current.value;
+        const newitem = { complete: false, text }
+        settodos([...todos, newitem]);
+
+        input.current.value = '';
+
+    }
+
+
+    const done =
+        (index) => {
+            const newtodos = [...todos];
+            newtodos[index].complete = !newtodos[index].complete
+            settodos(newtodos)
+        }
+    const deleted =
+        (index) => {
+            const newtodos = [...todos];
+            newtodos.splice(index, 1)
+            settodos(newtodos)
+
+        }
+
+
+
+    return (
+        <>
+            <div className='container todo'>
+                <div className='all'>
+                    <h1 className='text-center text-capitalize'>todo</h1>
+                    <form >
+                        <div className='inputs row ' >
+                            <div className='col-md-9 col-sm-12'>
+                                <input name='nametask' ref={input} style={{ width: '70%' }} type='text' placeholder='Add a task.........' />
+                            </div>
+                            <div className='col-md-3 col-sm-12'>
+                                <button className='col-md-3' onClick={add} ref={addbtn} type='submit' className='ms-auto add'>Add a task</button>
+                            </div>    </div></form>
+
+                    {todos.map(({ text, complete }, index) => {
+                        return (
+                            <div className='tasks d-flex flex-row' >
+                                <h3 className={complete ? 'done' : ''} ref={parg}>{text}</h3>
+                                <div className='icons ms-auto'>
+                                    <span onClick={() => parg.className = done(index)}   >      <FontAwesomeIcon icon={faCheck} /> </span>
+                                    <span>     <FontAwesomeIcon icon={faPenToSquare} /> </span>
+                                    <span onClick={() => deleted(index)}><FontAwesomeIcon icon={faXmark} /> </span>
+                                </div>     </div>)
+                    })}
+
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+        </>
+    )
+}
+export default Todo
+
+
+
+
+
+
+
+
+
+
+
+
+
